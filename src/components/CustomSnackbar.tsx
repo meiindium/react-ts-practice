f2d3148da588d9d394301d0f0deaf2fdbacab821
meiindium <meiyarasu.bs@indiumsoft.com>
2024-05-03 15:13:54 +0530

    Implemented in Grid for all screen res

--- src/components/CustomSnackbar.tsx
+++ src/components/CustomSnackbar.tsx
@@ -6,6 +6,7 @@ import {
   LinearProgress,
   Typography,
   Box,
+  Grid,
 } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import CloudCircleIcon from "@mui/icons-material/CloudCircle";
@@ -26,23 +27,23 @@ const CustomSnackbar: React.FC<CustomSnackbarProps> = ({
   useEffect(() => {
     let timer: NodeJS.Timeout;
 
-    if (open) {
-      timer = setInterval(() => {
-        setProgress((prevProgress) => {
-          if (prevProgress >= 100) {
-            clearInterval(timer);
-            handleClose();
-            return 0;
-          } else {
-            return prevProgress + 10;
-          }
-        });
-      }, 300);
-    }
+    // if (open) {
+    //   timer = setInterval(() => {
+    //     setProgress((prevProgress) => {
+    //       if (prevProgress >= 100) {
+    //         clearInterval(timer);
+    //         handleClose();
+    //         return 0;
+    //       } else {
+    //         return prevProgress + 10;
+    //       }
+    //     });
+    //   }, 300);
+    // }
 
-    return () => {
-      clearInterval(timer);
-    };
+    // return () => {
+    //   clearInterval(timer);
+    // };
   }, [open, handleClose]);
 
   const handleUploadCancel = () => {
@@ -72,80 +73,95 @@ const CustomSnackbar: React.FC<CustomSnackbarProps> = ({
     >
       <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
         <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
+        // sx={{
+        //   display: "flex",
+        //   alignItems: "center",
+        //   justifyContent: "space-between",
+        // }}
         >
-          <Typography
-            sx={{
-              color: "#fff",
-              fontSize: 16,
-              textAlign: "center",
-              fontWeight: 300,
-            }}
-          >
-            Upload Progress
-          </Typography>
-          <IconButton
-            size="small"
-            aria-label="close"
-            color="inherit"
-            onClick={handleUploadCancel}
-          >
-            <CloseIcon sx={{ color: "#fff" }} />
-          </IconButton>
+          <Grid container>
+            <Grid item xs={11}>
+              <Typography
+                sx={{
+                  color: "#fff",
+                  fontSize: 16,
+                  fontWeight: 300,
+                  textAlign: "left",
+                }}
+              >
+                Upload Progress
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleUploadCancel}
+              >
+                <CloseIcon sx={{ color: "#fff" }} />
+              </IconButton>
+            </Grid>
+          </Grid>
         </Box>
-        <Divider
-          sx={{
-            marginBottom: 1,
-            marginInline: "-20px",
-            borderColor: "#959595",
-          }}
-        />
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            marginBottom: 1,
-            gap: 1,
-          }}
+        <Grid container>
+          <Grid item xs={12}>
+            <Divider
+              sx={{
+                marginBottom: 1,
+                marginInline: "-20px",
+                borderColor: "#959595",
+              }}
+            />
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          // sx={{
+          //   display: "flex",
+          //   alignItems: "center",
+          //   marginBottom: 1,
+          //   gap: 1,
+          // }}
         >
-          <CloudCircleIcon sx={{ color: "#fff" }} />
-          <Typography sx={{ color: "#fff", fontSize: 16 }}>PLA2356</Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box sx={{ width: 175 }}>
+          <Grid item xs={1}>
+            <CloudCircleIcon sx={{ color: "#fff" }} />
+          </Grid>
+          <Grid item xs={3}>
+            <Typography sx={{ color: "#fff", fontSize: 16 }}>
+              PLA2356
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container sx={{ display: "flex", alignItems: "center" }}>
+          <Grid item xs={6}>
             <LinearProgress
               variant="determinate"
               value={progress}
               sx={{ flex: 1, borderRadius: 20, height: 5 }}
             />
-          </Box>
+          </Grid>
 
-          <Typography style={{ color: "#fff", fontSize: 14, gap: 1 }}>
-            {`${progress}%`}
-          </Typography>
-          
-          <Typography
-            sx={{
-              marginLeft: 1,
-              color: "#fff",
-              fontSize: 18,
-              cursor: "pointer",
-            }}
-            onClick={handleUploadCancel}
-          >
-            Cancel
-          </Typography>
-        </Box>
+          <Grid item xs={2}>
+            <Typography style={{ color: "#fff", fontSize: 14, gap: 1 }}>
+              {`${progress}%`}
+            </Typography>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Typography
+              sx={{
+                marginLeft: 1,
+                color: "#fff",
+                fontSize: 18,
+                cursor: "pointer",
+              }}
+              onClick={handleUploadCancel}
+            >
+              Cancel
+            </Typography>
+          </Grid>
+        </Grid>
       </Box>
     </Snackbar>
   );
